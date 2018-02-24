e1c8006c97bcf9da8bf4f0376591f2cde6da1ade
hong <1083178465@qq.com>
2018-02-24 22:45:05 +0800

    增加广告的redis缓存

--- src/main/java/cn/scene/controller/AdvController.java
+++ src/main/java/cn/scene/controller/AdvController.java
@@ -5,7 +5,6 @@ import cn.scene.service.AdvService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
--- src/main/java/cn/scene/serviceImpl/AdvServiceImpl.java
+++ src/main/java/cn/scene/serviceImpl/AdvServiceImpl.java
@@ -1,9 +1,13 @@
 package cn.scene.serviceImpl;
 
 import cn.scene.dao.AdvertiseMapper;
+import cn.scene.jedis.JedisClient;
 import cn.scene.model.Advertise;
 import cn.scene.service.AdvService;
+import cn.scene.util.JsonUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,8 +20,27 @@ public class AdvServiceImpl implements AdvService{
 
     @Autowired
     private AdvertiseMapper advertiseMapper;
+    @Autowired
+    private JedisClient jedisClient; //redis客户端
+    @Value("${INDEX_ADV}")
+    private String INDEX_ADV;
 
     public List<Advertise> info() {
-        return advertiseMapper.selectInfo();
+        try{
+            String adv = jedisClient.get(INDEX_ADV);
+            //判断不为空
+            if(StringUtils.isNotBlank(adv)){
+                return JsonUtils.jsonToList(adv,Advertise.class);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        List<Advertise> list =  advertiseMapper.selectInfo();
+        try{
+            jedisClient.set(INDEX_ADV,JsonUtils.objectToJson(list));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return list;
     }
 }
